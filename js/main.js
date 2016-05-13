f798d7d94e1d1b977ac9ef0b68b49b7468a801dc
shundroid <ra24483@qb3.so-net.ne.jp>
2016-05-13 20:57:13 +0900

    title部分を.vueとしてコンポーネントに

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
 var Vue = require("vue");
+import TodoTitle from "./todo-title.vue";
 
 document.addEventListener("DOMContentLoaded", function() {
-  new Vue({
+  var app = new Vue({
     el: "#app",
+    components: { TodoTitle },
     data: {
-      todos: [
-        "hoge",
-        "fuga"
-      ]
+      newTodo: "",
+      todos: []
+    },
+    methods: {
+      addTodo: function() {
+        var text = this.newTodo.trim();
+        if (text) {
+          this.todos.push({ text: text });
+          this.newTodo = "";
+        }
+      },
+      removeTodo: function(index) {
+        this.todos.splice(index, 1);
+      }
     }
   });
 });
